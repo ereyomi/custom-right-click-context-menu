f2cb5ca1d3860e860d94d1b61212dc094171e442
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2022-06-30 23:08:09 +0100

    feat: completed context

--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
+// @ts-nocheck
 document.addEventListener('DOMContentLoaded', () => {
   const contextMenu = document.querySelector('.ere-context-menu');
+  const subMenus = document.querySelectorAll('.subMenus');
   document.addEventListener('contextmenu', (event) => {
     event.preventDefault();
     let x = event.offsetX;
     let y = event.offsetY;
-    console.log(x, y);
-    // @ts-ignore
+    let winWidth = window.innerWidth;
+    let winHeight = window.innerHeight;
+    let cmWidth = contextMenu.offsetWidth;
+    let cmHeight = contextMenu.offsetHeight;
+    subMenus.forEach((subMenu) => {
+      if (x > winWidth - cmWidth - subMenu.offsetWidth) {
+        subMenu.style.left = '-100px';
+      } else {
+        subMenu.style.left = '';
+        subMenu.style.right = '-100px';
+      }
+    });
+    x = x > winWidth - cmWidth ? winWidth - cmWidth - 5 : x;
+    y = y > winHeight - cmHeight ? winHeight - cmHeight - 5 : y;
     contextMenu.style.left = `${x}px`;
-    // @ts-ignore
     contextMenu.style.top = `${y}px`;
+    contextMenu.style.visibility = 'visible';
+  });
+  document.addEventListener('click', () => {
+    contextMenu.style.visibility = 'hidden';
   });
 });
